a88241c579df203e50687aa4333f49b51020defe
Dominik Prediger <dominik.pre@gmail.com>
2021-01-30 15:14:55 +0100

    Add back to last screen and alertDialog for wrong files

--- lib/views/loadingjson.dart
+++ lib/views/loadingjson.dart
@@ -10,6 +10,28 @@ class LoadingJSON extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingJSON> {
+
+  void _alertDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Error"),
+          //TODO: give more comprehensive error
+          content: Text("Are you sure you have selected the OFA.zip file?"),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              }, 
+              child: Text("Try again"),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
   void readJSON() async {
   
     try {
@@ -24,12 +46,19 @@ class _LoadingScreenState extends State<LoadingJSON> {
       } else {
         //TODO: Give user info his json file was
         print('error1');
-        Navigator.pushReplacementNamed(context, '/noJSON');
+        // Navigator.pushReplacementNamed(context, '/noJSON');
+        Navigator.of(context).pop(_alertDialog());
+        
       }
     } catch (e) {
         //TODO: Give user info his json file was
       print(e);
-      Navigator.pushReplacementNamed(context, '/noJSON');
+      print('error2');
+      // Navigator.pushReplacementNamed(context, '/noJSON');
+      Navigator.of(context).pop();
+      _alertDialog();
+      
+
     }
   }
 
