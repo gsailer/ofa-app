08922616113bca942d8ac834be4ff20bc815bf03
Dominik Prediger <dominik.pre@gmail.com>
2021-02-17 12:22:11 +0100

    Add layout features and fix some bugs

--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -9,7 +9,10 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoard extends State<DashBoard> {
   OFAjson data;
-
+    OFAjson sortedData;
+    ListView subtitleCollapsed;
+    ListView subtitle ;
+    int firstInit = 1;
   @override
   void initState() {
     super.initState();
@@ -17,9 +20,11 @@ class _DashBoard extends State<DashBoard> {
 
   @override
   Widget build(BuildContext context) {
-    data = ModalRoute.of(context).settings.arguments;
 
+   data = ModalRoute.of(context).settings.arguments;
+   sortedData = sortData(data);
     return Scaffold(
+      backgroundColor: Color(0xff212121),
       appBar: AppBar(
         backgroundColor: Color(0xff212121),
         leading: IconButton(
@@ -34,12 +39,17 @@ class _DashBoard extends State<DashBoard> {
         ),
       ),
       body: ListView(
-        // physics: const AlwaysScrollableScrollPhysics(),
-        // shrinkWrap: true,
-        children: <Widget>[
-          _appWebCardWindow(),
-        ],
-      ),
+            // physics: const AlwaysScrollableScrollPhysics(),
+            // shrinkWrap: true,
+            children: <Widget>[
+              //TODO get the right amount of websites and apps 
+              _appsAndWebsitesCard(),
+              _appWebCardWindow("Web-sites that share your information (${data.offFacebookActivity.length})"),
+              _appWebCardWindow("Apps that share your information (${data.offFacebookActivity.length})"),
+            ],
+          ),
+      
+      
     );
   }
 
@@ -51,47 +61,73 @@ class _DashBoard extends State<DashBoard> {
     //TODO This should be the content of the insight cards. When clicking on one of the cards, the corresponding insight screen should be displayed.
   }
 
-  Widget _appWebCardWindow() {
-    OFAjson sortedData = sortData(data);
-    //TODO The window where the app and website Cards with most shared infos to FB are displayed
-    return ExpansionTile(
-      title: Text(
-        "Web-sites that share your information (${data.offFacebookActivity.length})",
-      ),
-      subtitle: Container(
-        child: ListView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: (data.offFacebookActivity.length > 2)
-              ? 2
-              : data.offFacebookActivity.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(sortedData.offFacebookActivity[index].name),
-              subtitle: Text(sortedData.offFacebookActivity[index].events.length
-                      .toString() +
-                  ' Events'),
-            );
-          },
+  Widget _appWebCardWindow(String title) {
+    if(firstInit == 1){
+      subtitle = _subtitle(data, sortedData);
+      firstInit --;
+    }
+    //TODO Let the tile take up the whole screen when expanded (user should still be able to scroll to other tiles) and collapse other tiles
+    return Theme(
+      data: Theme.of(context).copyWith(unselectedWidgetColor: Colors.white, accentColor: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 8.0),
+        child: ListTileTheme(
+            dense: true, //TODO See if this needs to be here
+            contentPadding: EdgeInsets.symmetric(horizontal: 4),
+                      child: ExpansionTile(
+                    title: Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    onExpansionChanged: (state){
+                      setState(() {
+                        subtitle = state ? null : _subtitle(data, sortedData);  
+                      });
+                    },
+                    subtitle: subtitle,
+                    children: <Widget>[
+                      ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: (data.offFacebookActivity.length > 2)
+                            ? data.offFacebookActivity.length - 2
+                            : 0,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 4
+                            ),
+                                                      child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 1,
+                                )
+                              ),
+                              child: ListTile(
+                                  leading: Image(
+                                    image: AssetImage('assets/images/3_todo.png'),
+                                    width: 44,
+                                    height: 44,
+                                  ),
+                                  title: Text(sortedData.offFacebookActivity[index].name,
+                                  style: TextStyle(color: Colors.white),),
+                                  subtitle: Text(sortedData.offFacebookActivity[index].events.length.toString() +' Events',
+                                  style: TextStyle(color: Colors.white),),
+                                ),
+                            ),
+                          );
+                          
+                        },
+                      )
+                    ],
+                  ),
+          ),
         ),
-      ),
-      children: <Widget>[
-        ListView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: (data.offFacebookActivity.length > 2)
-              ? data.offFacebookActivity.length - 2
-              : 0,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(sortedData.offFacebookActivity[index].name),
-              subtitle: Text(sortedData.offFacebookActivity[index].events.length
-                      .toString() +
-                  ' Events'),
-            );
-          },
-        )
-      ],
     );
   }
 
@@ -104,4 +140,43 @@ OFAjson sortData(OFAjson data) {
   data.offFacebookActivity
       .sort((a, b) => b.events.length.compareTo(a.events.length));
   return data;
+}
+
+ListView _subtitle(data, sortedData){
+  return ListView.builder(
+    physics: const NeverScrollableScrollPhysics(),
+    shrinkWrap: true,
+    itemCount: (data.offFacebookActivity.length > 2)
+        ? 2
+        : data.offFacebookActivity.length,
+    itemBuilder: (context, index) {
+      return Padding(
+        padding: const EdgeInsets.only(
+          top: 8
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white,
+              width: 1,
+            )
+          ),
+          child: ListTile(
+              leading: Image(
+                image: AssetImage('assets/images/3_todo.png'),
+                width: 44,
+                height: 44,
+              ),
+              title: Text(sortedData.offFacebookActivity[index].name,
+              style: TextStyle(
+                color: Colors.white
+                )
+              ),
+              subtitle: Text(sortedData.offFacebookActivity[index].events.length.toString() + ' Events',
+                style: TextStyle(color: Colors.white),)
+            ),
+        ),
+      );
+    },
+  );
 }
