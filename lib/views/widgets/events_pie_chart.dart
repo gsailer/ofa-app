c7a51e8a0e090f3cbf510109cebdcb14344fe286
Dominik Prediger <dominik.pre@gmail.com>
2021-03-09 11:36:09 +0100

    Change size of piechart so text fits better

--- lib/views/widgets/events_pie_chart.dart
+++ lib/views/widgets/events_pie_chart.dart
@@ -4,6 +4,9 @@ import 'dart:math';
 /// the middle.
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
+import 'package:charts_common/common.dart' as common
+  show LayoutConfig,
+        MarginSpec;
 
 class DonutAutoLabelChart extends StatelessWidget {
   final List<charts.Series> seriesList;
@@ -14,20 +17,39 @@ class DonutAutoLabelChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new charts.PieChart(seriesList,
+    layoutConfig: new charts.LayoutConfig(
+      leftMarginSpec: common.MarginSpec.fixedPixel(0), 
+      topMarginSpec: common.MarginSpec.fixedPixel((MediaQuery.of(context).size.width * 0.03).round()), 
+      rightMarginSpec: common.MarginSpec.fixedPixel(0), 
+      bottomMarginSpec: common.MarginSpec.fixedPixel(0),
+      // (MediaQuery.of(context).size.width * 0.35).round()
+    ),
+    
+    
         animate: animate,
         defaultRenderer: new charts.ArcRendererConfig(
-            arcWidth: 80,
-            startAngle: -pi/2 + 0.05,
-            arcRendererDecorators: [new charts.ArcLabelDecorator(
-                 insideLabelStyleSpec: new charts.TextStyleSpec(
-                   color: charts.MaterialPalette.black,
-                   fontSize: 13
-                   ),
-                 outsideLabelStyleSpec: new charts.TextStyleSpec(
-                   color: charts.MaterialPalette.white,
-                   fontSize: 12
-                   )),
-                   ]
-                   ));
+
+            strokeWidthPx: 1,
+            arcWidth: 20,
+            startAngle: -pi / 2 + 0.05,
+            // arcRatio: 0.5,
+            arcRendererDecorators: [
+              new charts.ArcLabelDecorator(
+
+                labelPadding: 1,
+                leaderLineStyleSpec: charts.ArcLabelLeaderLineStyleSpec(
+
+                  length: 8,
+                  thickness: 1,
+                  color: charts.ColorUtil.fromDartColor(Colors.white),
+                ),
+                  labelPosition: charts.ArcLabelPosition.outside,
+                  insideLabelStyleSpec: new charts.TextStyleSpec(
+                    lineHeight: 10,
+                      color: charts.MaterialPalette.black, fontSize: 13),
+                  outsideLabelStyleSpec: new charts.TextStyleSpec(
+                    lineHeight: 1,
+                      color: charts.MaterialPalette.white, fontSize: 12)),
+            ]));
   }
 }
